c8333803fb05f4a74e3804317318f0206a9b6272
Diego Guerra <diego.guerra.suarez@gmail.com>
2016-06-04 23:37:23 +0200

    add easy access methods

--- lib/feedson.rb
+++ lib/feedson.rb
@@ -14,5 +14,24 @@ require 'feedson/formats/atom_config'
 require 'feedson/formats/rss_config'
 
 module Feedson
-  # Your code goes here...
+  def self.dump(json_data: nil)
+    converter = Feedson::JsonToFeed.new(json_data)
+    converter.to_xml
+  end
+
+  def self.parse(feed_contents: nil, format: :rss)
+    converter = Feedson::FeedToJson.new(feed_contents, doc_config: doc_config(format))
+    converter.as_json
+  end
+
+  private
+
+  def self.doc_config(format)
+    case format
+    when :itunes then Formats::ItunesRssConfig.new
+    when :atom then Formats::AtomConfig.new
+    else RssConfig.new
+    end
+  end
+
 end
--- spec/feedson_spec.rb
+++ spec/feedson_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe Feedson do
+  it "converts an xml to json" do
+    json_content = Feedson.parse(
+      feed_contents: File.open("spec/examples/itunes.xml"),
+      format: :itunes)
+    expect(json_content.keys).to eql(["rss"])
+  end
+
+  it "converts json to xml" do
+    xml = Feedson.dump(json_data: {"rss": {}})
+    expect(xml).to match("<rss")
+
+  end
 end
